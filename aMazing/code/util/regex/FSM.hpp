3d416d7fec2990bb3e80e134db011b8385e24a5f
AntiMoron <anti2moron@gmail.com>
2015-06-01 21:47:25 +0800

    Add FSM

---
+++ aMazing/code/util/regex/FSM.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include"Nodes.hpp"
+
+namespace aMazing
+{
+	class FSM
+	{
+	public:
+		void addNode()
+		{
+			;
+		}
+		void clear()
+		{
+			nodes.clear();
+		}
+		bool bind(int id_a,int id_b)
+		{
+			nodes[id_a];
+		}
+	private:
+		Node::NodeQueue nodes;
+	};
+}
--- aMazing/code/util/regex/Nodes.hpp
+++ aMazing/code/util/regex/Nodes.hpp
@@ -1,9 +1,40 @@
 #pragma once
+#include"../../common/CommonDef.hpp"
+#include<memory>
+#include<vector>
 
 namespace aMazing
 {
-	class Nodes
+	class Node
 	{
-		;
+	public:
+		typedef size_t size_type;
+		typedef std::vector<std::shared_ptr<Node> > NodeQueue;
+		void setIdentifer(int id)
+		{
+			identifer = id;
+		}
+		int getIdentifer() const
+		{
+			return identifer;
+		}
+		void clearNeighbors()
+		{
+			neighbors.clear();
+		}
+		void addNeighbor(const std::shared_ptr<Node>& node)
+		{
+			neighbors.push_back(node);
+		}
+		const std::shared_ptr<Node>& operator [] (size_type index) const 
+		{
+			return neighbors[index];
+		}
+	private:
+		//The identifer of the node.
+		//!!!note that in one node set,only one node with specific identifer can exist.
+		int identifer;
+		//The neighbors that have bridge with current node
+		NodeQueue neighbors;
 	};
 }
